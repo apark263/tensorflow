154a9e6085b937fb26ad222a84d36244bb8c1b9a
vishalsubbiah <vishal@cerebras.net>
2018-08-24 11:14:15 -0700

    most other files

--- tensorflow/core/common_runtime/direct_session.h
+++ tensorflow/core/common_runtime/direct_session.h
@@ -49,6 +49,7 @@ namespace tensorflow {
 
 class CostModel;
 class DebugGateway;
+class XlaExtractor;
 class Device;
 class DirectSessionFactory;
 
@@ -395,6 +396,7 @@ class DirectSession : public Session {
 
   // EXPERIMENTAL: debugger (tfdbg) related
   friend class DebugGateway;
+  friend class XlaExtractor;
 };
 
 }  // end namespace tensorflow
