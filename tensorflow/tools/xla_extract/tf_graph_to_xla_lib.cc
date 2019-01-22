4f30fd0c28fa8a6e44b57e47ea5dca2383d8583e
Vishal Subbiah <vishal@cerebras.net>
2019-01-22 11:47:08 -0800

    fixing name issue for simfab

--- tensorflow/tools/xla_extract/tf_graph_to_xla_lib.cc
+++ tensorflow/tools/xla_extract/tf_graph_to_xla_lib.cc
@@ -38,18 +38,19 @@ std::vector<XlaCompiler::Argument> BuildXlaArgsFromClientGraph(
       // iterate over the inputs to this node for the args
       for (const Node* in : node->in_nodes()) {
         auto in_def = in->def();
-	XlaCompiler::Argument arg;
-	if (in_def.op() == "VarHandleOp") {
-	  arg.kind = XlaCompiler::Argument::kResource;
-	  arg.resource_kind = XlaResource::kVariable;
-	  arg.initialized = true;
-	  GetNodeAttr(in_def, "shape", &(arg.shape));
-	} else {
-	  arg.kind = XlaCompiler::Argument::kParameter;
+	      XlaCompiler::Argument arg;
+	      if (in_def.op() == "VarHandleOp") {
+	        arg.kind = XlaCompiler::Argument::kResource;
+	        arg.resource_kind = XlaResource::kVariable;
+	        arg.initialized = true;
+	        GetNodeAttr(in_def, "shape", &(arg.shape));
+	      }
+        else {
+	        arg.kind = XlaCompiler::Argument::kParameter;
           std::vector<tensorflow::TensorShape> shape_value;
           GetNodeAttr(in_def, "_output_shapes", &shape_value);
           arg.shape = shape_value[0];
-	}
+	      }
         arg.name = in_def.name();
         GetNodeAttr(in_def, "dtype", &(arg.type));
         xla_args.push_back(std::move(arg));
@@ -121,12 +122,51 @@ xla::HloModuleProto ExtractHloFromGraphDef(const GraphDef& in_graph,
 
     s = compiler.CompileFunction(XlaCompiler::CompileOptions(), function,
                                  xla_args, &result);
-    if (!s.ok()) LOG(FATAL) << "Couldn't compile to xla" << s.error_message();
+    if (!s.ok()) LOG(FATAL) << "Couldn't compile to xla: " << s.error_message();
 
     LOG(INFO) << "Done Compiling";
     hmod.CopyFrom(result.computation->proto());
 
-    // name swapping
+    // hlo optimizations
+
+    // getting hlo_module from proto
+    xla::StatusOr<xla::ProgramShape> program_shape_status = result.computation->GetProgramShape();
+    xla::ProgramShape program_shape = program_shape_status.ValueOrDie();
+    xla::HloModuleConfig module_config = xla::HloModuleConfig(program_shape);
+    
+    xla::StatusOr<std::unique_ptr<xla::HloModule>> hlo_module_status = xla::HloModule::CreateFromProto(hmod, module_config);
+    std::unique_ptr<xla::HloModule> hlo_module = std::move(hlo_module_status.ValueOrDie());
+    std::cout<<hlo_module->name()<<"\n"; // can be removed in the future once build is stable
+
+    xla::HloPassPipeline pipeline("Interpreter");
+    // adding passes we wish to run
+    pipeline.AddPass<xla::CallInliner>();
+    pipeline.AddPass<xla::HloSubcomputationUnification>();
+    pipeline.AddPass<xla::HloCSE>(false);
+
+    xla::AlgebraicSimplifierOptions options(
+         [](const xla::Shape&, const xla::Shape&) { return false; });
+    options.set_enable_dot_strength_reduction(false);
+    pipeline.AddPass<xla::AlgebraicSimplifier>(options);
+    pipeline.AddPass<xla::WhileLoopSimplifier>();
+    pipeline.AddPass<xla::ReshapeMover>();
+    pipeline.AddPass<xla::HloConstantFolding>();
+    pipeline.AddPass<xla::HloCSE>(true);
+    pipeline.AddPass<xla::LayoutAssignment>(
+      hlo_module.get()->mutable_entry_computation_layout(),
+      xla::LayoutAssignment::InstructionCanChangeLayout);
+    pipeline.AddPass<xla::HloDCE>();
+    pipeline.AddPass<xla::FlattenCallGraph>();
+
+    // hlo optimization run
+    s = pipeline.Run(hlo_module.get()).status();
+
+    if (!s.ok()) LOG(FATAL) << "Couldn't Run HloOptimization: " << s.error_message();
+
+    LOG(INFO) << "Done HLO Optimization\n";
+    hmod = hlo_module.get()->ToProto();
+
+    // restoring names
     std::size_t entry_ind = 0;
     bool found = false;
     for (std::size_t i = 0; i < (std::size_t)hmod.computations_size(); i++) {
@@ -152,47 +192,6 @@ xla::HloModuleProto ExtractHloFromGraphDef(const GraphDef& in_graph,
       instr->set_name(xla_args.at(instr->parameter_number()).name);
 
     }
-   
-   LOG(INFO) << "name swapping done";
-      
-    // hlo optimizations
-
-    // getting hlo_module from proto
-    xla::StatusOr<xla::ProgramShape> program_shape_status = result.computation->GetProgramShape();
-    xla::ProgramShape program_shape = program_shape_status.ValueOrDie();
-    xla::HloModuleConfig module_config = xla::HloModuleConfig(program_shape);
-    
-    xla::StatusOr<std::unique_ptr<xla::HloModule>> hlo_module_status = xla::HloModule::CreateFromProto(hmod, module_config);
-    std::unique_ptr<xla::HloModule> hlo_module = std::move(hlo_module_status.ValueOrDie());
-    std::cout<<hlo_module->name()<<"\n"; // can be removed in the future once build is stable
-
-    xla::HloPassPipeline pipeline("Interpreter");
-    // adding passes we wish to run
-    pipeline.AddPass<xla::CallInliner>();
-    pipeline.AddPass<xla::HloSubcomputationUnification>();
-    pipeline.AddPass<xla::HloCSE>(false);
-
-    xla::AlgebraicSimplifierOptions options(
-        [](const xla::Shape&, const xla::Shape&) { return false; });
-    options.set_enable_dot_strength_reduction(false);
-    pipeline.AddPass<xla::AlgebraicSimplifier>(options);
-    pipeline.AddPass<xla::WhileLoopSimplifier>();
-    pipeline.AddPass<xla::ReshapeMover>();
-    pipeline.AddPass<xla::HloConstantFolding>();
-    pipeline.AddPass<xla::HloCSE>(true);
-    pipeline.AddPass<xla::LayoutAssignment>(
-      hlo_module.get()->mutable_entry_computation_layout(),
-      xla::LayoutAssignment::InstructionCanChangeLayout);
-    pipeline.AddPass<xla::HloDCE>();
-    pipeline.AddPass<xla::FlattenCallGraph>();
-
-    // hlo optimization run
-    s = pipeline.Run(hlo_module.get()).status();
-
-    if (!s.ok()) LOG(FATAL) << "Couldn't Run HloOptimization" << s.error_message();
-
-    LOG(INFO) << "Done HLO Optimization\n";
-    hmod = hlo_module.get()->ToProto();
   }
   if (device_mgr != nullptr) {
     delete(device_mgr);
